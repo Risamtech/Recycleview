45d7533fcb4cabc3f9f7f7200860591ff6463874
LATITUDE 7390 2 IN 1 <santosarisam97@gmail.com>
2022-06-30 21:22:32 +0700

    Recycleview App

---
+++ app/src/main/java/com/risamsantosa/recycleview/Profile.kt
@@ -0,0 +1,21 @@
+package com.risamsantosa.recycleview
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.risamsantosa.recycleview.databinding.ActivityProfileBinding
+
+class Profile : AppCompatActivity() {
+    private lateinit var binding: ActivityProfileBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnback.setOnClickListener{
+            val intent = Intent (this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
